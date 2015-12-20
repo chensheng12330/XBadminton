f67bea67a02ffbe94cfd2811e57efd9fa1b13789
chensheng12330 <chensheng12330@gmail.com>
2015-12-20 22:19:25 +0800

    Initial Commit

---
+++ XBadminton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBadminton
+//
+//  Created by sherwin.chen on 15/12/20.
+//  Copyright (c) 2015年 sherwin.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XBadminton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XBadminton
+//
+//  Created by sherwin.chen on 15/12/20.
+//  Copyright (c) 2015年 sherwin.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
